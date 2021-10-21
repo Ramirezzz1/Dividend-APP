da28db305a90122d0be48d1fefe1b3141a50b751
Ramirezz1 <ronaldramirezzz1@gmail.com>
2021-10-20 22:25:43 -0400

    home text and button issues

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -1,14 +1,31 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Video from '../../videos/video.mp4'
-import { HeroContainer, HeroBg, VideoBg } from './HeroElements';
+import { Button } from '../ButtonElement';
+import { HeroContainer, HeroBg, VideoBg,HeroContent,HeroH1,HeroP,HeroBtnWrapper,ArrowForward,ArrowRight } from './HeroElements';
 
 
 const HeroSection = () => {
+    const [hover,setHover] = useState(false)
+
+    const onHover= () => {
+        setHover(!hover)
+    }
     return (
         <HeroContainer id="home">
             <HeroBg>
                 <VideoBg autoPlay loop muted src={Video} type='video/mp4'/>
             </HeroBg>
+            <HeroContent>
+                <HeroH1>Track your Portfolio for Free!</HeroH1>
+                <HeroP>
+                    Join us by Signing up
+                </HeroP>
+                <HeroBtnWrapper>
+                    <Button to="signup" onMouseEnter={onHover} onMouseLeave={onHover}>
+                        Get started {hover ? <ArrowForward /> : <ArrowRight />}
+                    </Button>
+                </HeroBtnWrapper>
+            </HeroContent>
         </HeroContainer>
     )
 }
