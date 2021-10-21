0924f16495629763e8ed1575a76f679cc5562cf2
Ramirezz1 <ronaldramirezzz1@gmail.com>
2021-10-20 21:31:19 -0400

    video background bugs

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,18 +1,18 @@
 import React from 'react'
 import { SidebarContainer, Icon, CloseIcon,SidebarWrapper,SidebarMenu,SidebarLink, SideBtnWrap, SidebarRoute } from './SidebarElements'
 
-const Sidebar = () => {
+const Sidebar = ({isOpen,toggle}) => {
     return (
-        <SidebarContainer>
-            <Icon>
+        <SidebarContainer isOpen={isOpen} onClick={toggle}>
+            <Icon onClick={toggle}>
                 <CloseIcon />
             </Icon>
             <SidebarWrapper>
                 <SidebarMenu>
-                    <SidebarLink to="about">About</SidebarLink>
-                    <SidebarLink to="discover">Discover</SidebarLink>
-                    <SidebarLink to="Services">Services</SidebarLink>
-                    <SidebarLink to="signup">SignUp</SidebarLink>
+                    <SidebarLink to="about" onClick={toggle}>About</SidebarLink>
+                    <SidebarLink to="discover" onClick={toggle}>Discover</SidebarLink>
+                    <SidebarLink to="Services" onClick={toggle}>Services</SidebarLink>
+                    <SidebarLink to="signup" onClick={toggle}>SignUp</SidebarLink>
                 </SidebarMenu>
                 <SideBtnWrap>
                     <SidebarRoute to="/signin">Sign In</SidebarRoute>
