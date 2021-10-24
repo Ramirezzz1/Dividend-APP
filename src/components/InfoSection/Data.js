ab5df0587e8c1b51c4aaec8f58af54881b6f204c
Ramirezz1 <ronaldramirezzz1@gmail.com>
2021-10-23 23:32:28 -0400

    images missing

--- src/components/InfoSection/Data.js
+++ src/components/InfoSection/Data.js
@@ -3,7 +3,7 @@ export const homeObjOne = {
     lightBg: false,
     lightText: true,
     lightTextDesc: true,
-    topLine: 'Stock Portfolio',
+    topLine: 'Your Portfolio',
     headline:'Keep track of your portfolio for free',
     description:'Users are able to create multiple portfolios in order to track their investments in real time and create a portfolio to simiulate their financial goals. ',
     buttonLabel: 'Get started',
--- src/components/InfoSection/index.js
+++ src/components/InfoSection/index.js
@@ -1,8 +1,8 @@
 import React from 'react'
-import { Button } from 'react-scroll'
-import { InfoContainer, InfoWrapper, InfoRow, Column1, Column2, TextWrapper, TopLine, Heading, Subtitle, BtnWrap, ImgWrap, Img } from './InfoElements'
+import { Button } from '../ButtonElement';
+import { InfoContainer, InfoWrapper, InfoRow, Column1, Column2, TextWrapper, TopLine, Heading, Subtitle, BtnWrap, ImgWrap, Img } from './InfoElement';
 
-const InfoSection = () => {
+const InfoSection = ({lightBg, id, imgStart,topLine,lightText,darkText,headline,description,buttonLabel,img,alt}) => {
     return (
         <>
         <InfoContainer lightBg={lightBg} id={id}>
@@ -10,17 +10,17 @@ const InfoSection = () => {
                 <InfoRow imgStart={imgStart}>
                     <Column1>
                      <TextWrapper>
-                         <TopLine>{TopLine}</TopLine>
-                         <Heading>{headline}</Heading>
-                         <Subtitle>Subtitle</Subtitle>
+                         <TopLine>{topLine}</TopLine>
+                         <Heading lightText={lightText}>{headline}</Heading>
+                         <Subtitle darkText={darkText}>{description}</Subtitle>
                          <BtnWrap>
-                             <Button to="home"/>
+                             <Button to="home">{buttonLabel}</Button>
                          </BtnWrap>
                      </TextWrapper>
                     </Column1>
                     <Column2>
                      <ImgWrap>
-                      <Img/>
+                      <Img src={img} alt={alt}/>
                      </ImgWrap>
                     </Column2>
                 </InfoRow>
@@ -31,4 +31,4 @@ const InfoSection = () => {
     )
 }
 
-export default InfoSection
+export default InfoSection;
