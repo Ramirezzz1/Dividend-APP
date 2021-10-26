6d563389b3e3ca77aa776d62cc5c08d5051771b5
Ramirezz1 <ronaldramirezzz1@gmail.com>
2021-10-25 20:52:21 -0400

    footer

--- src/components/InfoSection/Data.js
+++ src/components/InfoSection/Data.js
@@ -25,12 +25,12 @@ export const homeObjTwo = {
     headline:'Keep track of your portfolio for free',
     description:'Users are able to create multiple portfolios in order to track their investments in real time and create a portfolio to simiulate their financial goals. ',
     buttonLabel: 'Get started',
-    imgStart: false,
+    imgStart: true,
     img: require('../../images/pic3.png').default,
-    alt: 'pic1',
-    dark: true,
-    primary: true,
-    darkText: false
+    alt: 'pic2',
+    dark: false,
+    primary: false,
+    darkText: true
 
 };
 
@@ -42,7 +42,7 @@ export const homeObjThree = {
     topLine: 'Your Portfolio',
     headline:'Keep track of your portfolio for free',
     description:'Users are able to create multiple portfolios in order to track their investments in real time and create a portfolio to simiulate their financial goals. ',
-    buttonLabel: 'Get started',
+    buttonLabel: 'Learn More',
     imgStart: false,
     img: require('../../images/pic4.png').default,
     alt: 'pic1',
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,8 @@ import Navbar from '../components/Navbar';
 import HeroSection from '../components/HeroSection';
 import InfoSection from '../components/InfoSection';
 import { homeObjOne,homeObjTwo, homeObjThree } from '../components/InfoSection/Data';
+import Services from '../components/Services';
+import Footer from '../components/Footer/FooterElements';
 
 
 const Home = () => {
@@ -21,7 +23,9 @@ const Home = () => {
           <HeroSection />
           <InfoSection {...homeObjOne}/>
           <InfoSection {...homeObjTwo}/>
+          <Services />
           <InfoSection {...homeObjThree}/>
+          <Footer />
         </>
     );
 };
